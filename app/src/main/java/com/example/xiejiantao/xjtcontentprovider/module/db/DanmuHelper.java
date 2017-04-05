aa91920bae7650a8fde74ca044157346d5c55866
xiejiantao <xiejiantao@corp.netease.com>
2017-04-05 16:04:09 +0800

    add：helper

--- app/src/main/java/com/example/xiejiantao/xjtcontentprovider/module/db/DanmuHelper.java
+++ app/src/main/java/com/example/xiejiantao/xjtcontentprovider/module/db/DanmuHelper.java
@@ -1,9 +1,32 @@
 package com.example.xiejiantao.xjtcontentprovider.module.db;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+
 /**
  * Created by xiejiantao on 2017/4/5.
  */
 
 public class DanmuHelper {
-
+    public static long danmuTitleTime(Context context,String roomId,long currentTime) {
+        String where = MySqliteOpenHelper.DanmuColumns.ROOM_ID + "=?";
+        String[] args = {roomId};
+        Cursor cursor = context.getContentResolver().query(MySqliteOpenHelper.DanmuColumns.DANMU_CONTENT_URI, null, where, args, null);
+        if (cursor==null){
+            return currentTime;
+        }
+        if (cursor != null&&cursor.getCount()<=0){
+            cursor.close();
+            return currentTime;
+        }
+        if (cursor != null && cursor.moveToLast()) {
+            long preTime=cursor.getLong(cursor.getColumnIndex(MySqliteOpenHelper.DanmuColumns.TIME));
+            cursor.close();
+//            if (!Utils.isSameDay(preTime,currentTime)){
+                return currentTime;
+//            }
+        }
+        return 0;
+    }
 }
